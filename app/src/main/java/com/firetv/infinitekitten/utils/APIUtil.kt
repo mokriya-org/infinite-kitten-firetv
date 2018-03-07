eae47311261aefa7785f7a79e0c1f1458d6eda5a
Diogo Brito <diogowbrito@gmail.com>
2018-03-07 17:00:09 +0000

    Removed unnecessary type.

--- app/src/main/java/com/firetv/infinitekitten/utils/APIUtil.kt
+++ app/src/main/java/com/firetv/infinitekitten/utils/APIUtil.kt
@@ -2,5 +2,5 @@ package com.firetv.infinitekitten.utils
 
 object APIUtil {
     const val YOUTUBE_API_URL = "https://www.googleapis.com/youtube/v3/"
-    const val API_KEY: String = "AIzaSyBxYoA67eoGShOtrd9yhzAFO1p_xhLdEgs"
+    const val API_KEY = "AIzaSyBxYoA67eoGShOtrd9yhzAFO1p_xhLdEgs"
 }
