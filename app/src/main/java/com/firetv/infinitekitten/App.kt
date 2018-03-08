181c3c2208645604a93b7f053681ca11126dfaa2
Diogo Brito <diogowbrito@gmail.com>
2018-03-08 23:32:21 +0000

    Includes back YouTube sample code.

--- app/src/main/java/com/firetv/infinitekitten/App.kt
+++ app/src/main/java/com/firetv/infinitekitten/App.kt
@@ -3,15 +3,18 @@ package com.firetv.infinitekitten
 import android.app.Application
 import android.content.Context
 import android.util.Log
+import com.firetv.infinitekitten.api.ApiConstants
 import com.firetv.infinitekitten.api.flickr.FlickrApiService
 import com.firetv.infinitekitten.api.flickr.model.PhotoSizesResponse
 import com.firetv.infinitekitten.api.youtube.YouTubeApiService
+import com.firetv.infinitekitten.api.youtube.model.playlist.PlaylistItemsResponse
+import com.firetv.infinitekitten.api.youtube.model.video.VideosResponse
 import com.firetv.infinitekitten.utils.Constants
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import com.firetv.infinitekitten.api.flickr.model.SearchResponse as FlickrSearchList
-import com.firetv.infinitekitten.api.youtube.model.search.SearchResponse as YoutubeSearchList
+import com.firetv.infinitekitten.api.flickr.model.SearchResponse as FlickrSearchResponse
+import com.firetv.infinitekitten.api.youtube.model.search.SearchResponse as YoutubeSearchResponse
 
 class App : Application() {
     companion object {
@@ -32,46 +35,46 @@ class App : Application() {
         instance = this
 
         //TODO: Code used as sample to be integrated in future code. Delete when no longer required
-        /*    val playlistPage = youTubeApiService.getPageForPlaylist(playlistId = ApiConstants.CAT_PLAYLIST)
-            playlistPage.enqueue(object : Callback<PlaylistItemList> {
-                override fun onFailure(call: Call<PlaylistItemList>?, t: Throwable?) {
-                    Log.d(Constants.TAG, "getPageForPlaylist failed")
-                }
+        val playlistPage = youTubeApiService.getPageForPlaylist(playlistId = ApiConstants.CAT_PLAYLIST)
+        playlistPage.enqueue(object : Callback<PlaylistItemsResponse> {
+            override fun onFailure(call: Call<PlaylistItemsResponse>?, t: Throwable?) {
+                Log.d(Constants.TAG, "getPageForPlaylist failed")
+            }
 
-                override fun onResponse(call: Call<PlaylistItemList>?, response: Response<PlaylistItemList>?) {
-                    Log.d(Constants.TAG, "getPageForPlaylist success")
-                }
-            })
-            val searchPage = youTubeApiService.getPageForSearch(query = ApiConstants.CAT_SEARCH_QUERY, topicId = ApiConstants.CAT_SEARCH_TOPIC_ID)
-            searchPage.enqueue(object : Callback<YoutubeSearchList> {
-                override fun onFailure(call: Call<YoutubeSearchList>?, t: Throwable?) {
-                    Log.d(Constants.TAG, "getPageForSearch failed")
-                }
+            override fun onResponse(call: Call<PlaylistItemsResponse>?, response: Response<PlaylistItemsResponse>?) {
+                Log.d(Constants.TAG, "getPageForPlaylist success")
+            }
+        })
+        val searchPage = youTubeApiService.getPageForSearch(query = ApiConstants.CAT_SEARCH_QUERY, topicId = ApiConstants.CAT_SEARCH_TOPIC_ID)
+        searchPage.enqueue(object : Callback<YoutubeSearchResponse> {
+            override fun onFailure(call: Call<YoutubeSearchResponse>?, t: Throwable?) {
+                Log.d(Constants.TAG, "getPageForSearch failed")
+            }
 
-                override fun onResponse(call: Call<YoutubeSearchList>?, response: Response<YoutubeSearchList>?) {
-                    Log.d(Constants.TAG, "getPageForSearch success")
-                }
-            })
+            override fun onResponse(call: Call<YoutubeSearchResponse>?, response: Response<YoutubeSearchResponse>?) {
+                Log.d(Constants.TAG, "getPageForSearch success")
+            }
+        })
 
-            val videoInfo = youTubeApiService.getVideoInfo(videoId = "Ks-_Mh1QhMc")
-            videoInfo.enqueue(object : Callback<VideoList> {
-                override fun onFailure(call: Call<VideoList>?, t: Throwable?) {
-                    Log.d(Constants.TAG, "getVideoInfo failed")
-                }
+        val videoInfo = youTubeApiService.getVideoInfo(videoId = "Ks-_Mh1QhMc")
+        videoInfo.enqueue(object : Callback<VideosResponse> {
+            override fun onFailure(call: Call<VideosResponse>?, t: Throwable?) {
+                Log.d(Constants.TAG, "getVideoInfo failed")
+            }
 
-                override fun onResponse(call: Call<VideoList>?, response: Response<VideoList>?) {
-                    Log.d(Constants.TAG, "getVideoInfo success")
-                }
-            })*/
+            override fun onResponse(call: Call<VideosResponse>?, response: Response<VideosResponse>?) {
+                Log.d(Constants.TAG, "getVideoInfo success")
+            }
+        })
 
 
         val photos = flickrApiService.getPhotos(query = "kitten", page = 1)
-        photos.enqueue(object : Callback<FlickrSearchList> {
-            override fun onFailure(call: Call<FlickrSearchList>?, t: Throwable?) {
+        photos.enqueue(object : Callback<FlickrSearchResponse> {
+            override fun onFailure(call: Call<FlickrSearchResponse>?, t: Throwable?) {
                 Log.d(Constants.TAG, "getPhotos failed")
             }
 
-            override fun onResponse(call: Call<FlickrSearchList>?, response: Response<FlickrSearchList>?) {
+            override fun onResponse(call: Call<FlickrSearchResponse>?, response: Response<FlickrSearchResponse>?) {
                 Log.d(Constants.TAG, "getPhotos success")
             }
 
