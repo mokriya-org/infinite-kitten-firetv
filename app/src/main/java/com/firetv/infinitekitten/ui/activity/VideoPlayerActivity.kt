c0c2d638c209db5653d7355493288affce3ea52c
vinaykaitha <vinay.reddy.k.1989@gmail.com>
2018-03-12 19:00:02 +0530

    WiP: get next playlist when the current list ends

--- app/src/main/java/com/firetv/infinitekitten/ui/activity/VideoPlayerActivity.kt
+++ app/src/main/java/com/firetv/infinitekitten/ui/activity/VideoPlayerActivity.kt
@@ -3,10 +3,14 @@ package com.firetv.infinitekitten.ui.activity
 import android.os.Bundle
 import android.support.v4.app.FragmentActivity
 import com.devbrackets.android.exomedia.listener.VideoControlsSeekListener
+import com.devbrackets.android.playlistcore.data.PlaybackState
+import com.devbrackets.android.playlistcore.listener.PlaylistListener
 import com.firetv.infinitekitten.App
 import com.firetv.infinitekitten.R
 import com.firetv.infinitekitten.manager.PlaylistManager
+import com.firetv.infinitekitten.model.VideoPlaylistItem
 import com.firetv.infinitekitten.playlist.VideoMediaPlayerApi
+import com.firetv.infinitekitten.playlist.YouTubeMediaItemFetcher
 import kotlinx.android.synthetic.main.activity_video_player.*
 
 /**
@@ -16,6 +20,8 @@ class VideoPlayerActivity : FragmentActivity(), VideoControlsSeekListener {
 
     private lateinit var videoMediaPlayerApi: VideoMediaPlayerApi
     private val playlistManager: PlaylistManager = App.playlistManager
+    var nextPlayList: List<VideoPlaylistItem> = mutableListOf()
+    var playNextPlayList: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +34,7 @@ class VideoPlayerActivity : FragmentActivity(), VideoControlsSeekListener {
     override fun onStop() {
         super.onStop()
         playlistManager.removeVideoMediaPlayerApi(videoMediaPlayerApi)
+        playlistManager.unRegisterPlaylistListener(playListListener)
         playlistManager.invokeStop()
     }
 
@@ -51,7 +58,54 @@ class VideoPlayerActivity : FragmentActivity(), VideoControlsSeekListener {
         videoView.videoControls?.setSeekListener(this)
 
         videoMediaPlayerApi = VideoMediaPlayerApi(videoView)
+        videoView.setOnCompletionListener {
+            if(playNextPlayList){
+                playNextPlaylist()
+            }
+        }
         playlistManager.addVideoMediaPlayerApi(videoMediaPlayerApi)
+        playlistManager.registerPlaylistListener(playListListener)
+        playlistManager.play(0, false)
+    }
+
+    val playListListener = object : PlaylistListener<VideoPlaylistItem> {
+        override fun onPlaybackStateChanged(playbackState: PlaybackState): Boolean {
+            return false
+        }
+
+        override fun onPlaylistItemChanged(currentItem: VideoPlaylistItem?, hasNext: Boolean, hasPrevious: Boolean): Boolean {
+            playNextPlayList = !hasNext
+            if (!hasNext && nextPlayList.isEmpty()) {
+                loadNextPlaylist()
+            }
+            return false
+        }
+    }
+
+    private fun loadNextPlaylist() {
+        val youtubeMediaItemFetcher = YouTubeMediaItemFetcher(
+                context = App.context,
+                playlistId = playlistManager.playlistId,
+                pageToken = playlistManager.nextPageToken
+        )
+
+        youtubeMediaItemFetcher.fetch(object : YouTubeMediaItemFetcher.YouTubeMediaItemFetcherCallback {
+            override fun onSuccess(result: List<VideoPlaylistItem>, nextPageToken: String) {
+                val playlistManager = App.playlistManager
+                nextPlayList = result
+                if(nextPlayList.isNotEmpty()){
+                    playlistManager.nextPageToken = nextPageToken
+                }
+            }
+
+            override fun onFailure() {
+            }
+        })
+    }
+
+    private fun playNextPlaylist() {
+        playlistManager.setParameters(nextPlayList.toList(), 0)
+        nextPlayList = mutableListOf()
         playlistManager.play(0, false)
     }
 }
