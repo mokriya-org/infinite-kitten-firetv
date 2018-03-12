9e2df29d60e6c8661ead32186af0aad209c3224e
Dileepan Mohanachandran <dileepan@mokriya.com>
2018-03-13 00:47:03 +0530

    Cleaned up VideoLogManager

--- app/src/main/java/com/firetv/infinitekitten/manager/VideoLogManager.kt
+++ app/src/main/java/com/firetv/infinitekitten/manager/VideoLogManager.kt
@@ -2,7 +2,6 @@ package com.firetv.infinitekitten.manager
 
 import android.content.Context
 import com.firetv.infinitekitten.App
-import com.firetv.infinitekitten.api.ApiConstants
 
 /**
  * Created by dileepan on 12/03/18.
@@ -10,38 +9,23 @@ import com.firetv.infinitekitten.api.ApiConstants
 object VideoLogManager {
 
     private const val PREF_NAME = "prefInfiniteKitten"
-    private const val KEY_VIDEO_HISTORY_CATS = "keyVideoHistoryCats"
-    private const val KEY_VIDEO_HISTORY_HUMANS = "keyVideoHistoryHumans"
 
     private val prefs = App.context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
 
-    private val catVideoLogList = ArrayList<String>().apply {
-        addAll(prefs.getString(KEY_VIDEO_HISTORY_CATS, "").split(","))
+    fun addVideoId(videoId: String, playlistId: String) {
+        val logList = logListOf(playlistId)
+        logList.add(videoId)
+
+        prefs.edit().putString(keyForPlaylistId(playlistId), logList.joinToString(",")).apply()
     }
 
-    private val humanVideoLogList = ArrayList<String>().apply {
-        addAll(prefs.getString(KEY_VIDEO_HISTORY_HUMANS, "").split(","))
+    fun isSeen(videoId: String, playlistId: String): Boolean = logListOf(playlistId).contains(videoId)
+
+    fun clearLog(playlistId: String) = prefs.edit().putString(keyForPlaylistId(playlistId), "").apply()
+
+    private fun logListOf(playlistId: String) = ArrayList<String>().apply {
+        addAll(prefs.getString(keyForPlaylistId(playlistId), "").split(","))
     }
 
-    fun addVideoId(videoId: String, playlistId: String) = when (playlistId) {
-        ApiConstants.CAT_PLAYLIST -> {
-            catVideoLogList.add(videoId)
-            prefs.edit().putString(KEY_VIDEO_HISTORY_CATS, catVideoLogList.joinToString(",")).apply()
-        }
-        else -> {
-            humanVideoLogList.add(videoId)
-            prefs.edit().putString(KEY_VIDEO_HISTORY_HUMANS, humanVideoLogList.joinToString(",")).apply()
-        }
-    }
-
-    fun isSeen(videoId: String, playlistId: String): Boolean = when (playlistId) {
-        ApiConstants.CAT_PLAYLIST -> catVideoLogList.contains(videoId)
-        else -> humanVideoLogList.contains(videoId)
-    }
-
-    fun clearLog(playlistId: String) = prefs.edit().putString(when (playlistId) {
-        ApiConstants.CAT_PLAYLIST -> KEY_VIDEO_HISTORY_CATS
-        else -> KEY_VIDEO_HISTORY_HUMANS
-    }, "").apply()
-
+    private fun keyForPlaylistId(playlistId: String) = "key$playlistId"
 }
