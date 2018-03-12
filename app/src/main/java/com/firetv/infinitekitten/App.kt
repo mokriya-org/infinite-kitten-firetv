8b4831a70cfbc9bc651b9db907cdf34c2f6aae91
Diogo Brito <diogowbrito@gmail.com>
2018-03-12 12:04:42 +0000

    Includes Crashlytics.

--- app/src/main/java/com/firetv/infinitekitten/App.kt
+++ app/src/main/java/com/firetv/infinitekitten/App.kt
@@ -2,10 +2,13 @@ package com.firetv.infinitekitten
 
 import android.app.Application
 import android.content.Context
+import com.crashlytics.android.Crashlytics
 import com.firetv.infinitekitten.api.flickr.Flickr
 import com.firetv.infinitekitten.manager.PlaylistManager
+import io.fabric.sdk.android.Fabric
 import com.firetv.infinitekitten.api.flickr.model.SearchResponse as FlickrSearchResponse
 import com.firetv.infinitekitten.api.youtube.model.search.SearchResponse as YoutubeSearchResponse
+
 
 class App : Application() {
     companion object {
@@ -17,8 +20,9 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
         instance = this
-        playlistManager = PlaylistManager(this)
+        Fabric.with(instance, Crashlytics())
 
+        playlistManager = PlaylistManager(instance)
         Flickr.initUrls()
     }
 }
