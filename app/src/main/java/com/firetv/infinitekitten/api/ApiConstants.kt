b80a8f249ad4df7c645661d8ed0db6e719d71b1a
Diogo Brito <diogowbrito@gmail.com>
2021-01-18 11:20:33 +0000

    Updated Youtube API key. Updated Youtube playlist ids.

--- app/src/main/java/com/firetv/infinitekitten/api/ApiConstants.kt
+++ app/src/main/java/com/firetv/infinitekitten/api/ApiConstants.kt
@@ -2,7 +2,7 @@ package com.firetv.infinitekitten.api
 
 object ApiConstants {
     const val YOUTUBE_API_URL = "https://www.googleapis.com/youtube/v3/"
-    const val YOUTUBE_API_KEY = "AIzaSyBxYoA67eoGShOtrd9yhzAFO1p_xhLdEgs"
+    const val YOUTUBE_API_KEY = "AIzaSyClS8HjgfnyjyH44MwKeGzDU-DbfjFuVTQ"
     const val YOUTUBE_RESULTS_PER_PAGE = 15
 
     const val FLICKR_API_DOMAIN = "api.flickr.com"
@@ -10,6 +10,6 @@ object ApiConstants {
     const val FLICKR_API_KEY = "67ff8be480e8f7aeb0a12c691b624be0"
     const val FLICKR_RESULTS_PER_PAGE = 50
 
-    const val CAT_PLAYLIST = "PLxSTZt7tfw9-voNrcR6SZ-kP4rurxXhmI"
-    const val HUMAN_PLAYLIST = "PLxSTZt7tfw9_zrtS9W73pLon1tFv83y3b"
+    const val CAT_PLAYLIST = "PLonjapUvLQ7UDqv1BSHqhPnlxFxi46mdw"
+    const val HUMAN_PLAYLIST = "PLonjapUvLQ7VJBg8EkxDgDcuiHRcci_0Z"
 }
